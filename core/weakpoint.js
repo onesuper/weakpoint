a0c2e593d525a917aa804850a2398251b7b557ab
onesuper <onesuperclark@gmail.com>
2012-06-14 18:27:28 +0800

    list frame ok

--- core/weakpoint.js
+++ core/weakpoint.js
@@ -3,42 +3,98 @@
  * (c) 2012 onesuper
  */
 
+allSlides = document.querySelectorAll('section');
+//将所有的ul、ol的元素都隐藏起来
+hideAll();
 addEventListeners();   
-goPage(0);
-currentPage = 0;
+currentSlide = 0;
+goSlide(0);
+
 
 function addEventListeners() {
-    document.addEventListener('keydown', onDocumentKeyDown, false);
+    document.addEventListener('keydown', onKeyDown, false);
+    document.addEventListener('mousedown', onMouseDown, false);
 }
 
-function onDocumentKeyDown(event) {
+function onKeyDown(event) {
     switch(event.keyCode) {
-    case 33: case 37: previousPage();break;
-    case 34: case 39: nextPage();break;    
+    case 33: case 37: previousSlide();break;
+    case 34: case 39: nextSlide();break;    
+    case 36: goSlide(0);break;
     }
 }
 
-function previousPage() {
-    if (currentPage ==0)
-	return
-    var allPages = document.querySelectorAll('section');
-    allPages[currentPage].style.display = "none";
-    allPages[currentPage-1].style.display = "inline";
-    currentPage -= 1;
+function onMouseDown(event) {
+    switch(event.which) {
+    case 1: nextSlide();break;
+    }
 }
 
-function nextPage() {
-    var allPages = document.querySelectorAll('section');
-    if (currentPage == allPages.length - 1)
-	return
-    allPages[currentPage].style.display = "none";
-    allPages[currentPage+1].style.display = "inline";
-    currentPage += 1;
+function hideAll() {
+    var allLists = document.querySelectorAll('li');
+    for (var i=0; i<allLists.length; i++) {
+	allLists[i].style.display ="none";
+    }
+    var allP = document.querySelectorAll('p');
+    for (var i = 0; i < allP.length; i++) {
+	allP[i].style.display = "none";
+    }
 }
 
-function goPage(i) {
-    var allPages = document.querySelectorAll('section');
-    allPages[i].style.display = "inline";
-    currentPage = i;
+function previousSlide() {
+    if (currentSlide ==0)
+	return
+
+    var allLists = allSlides[currentSlide].querySelectorAll('li');
+    for (var i = allLists.length - 1; i >= 0; i--) {
+	if (allLists[i].style.display == "block") {
+	    allLists[i].style.display = "none";
+	    return;
+	}
+    }
+
+    var allP = allSlides[currentSlide].querySelectorAll('p');
+    for (var i = allP.length - 1; i >= 0; i--) {
+	if (allP[i].style.display == "block") {
+	    allP[i].style.display = "none";
+	    return;
+	}
+    }
+
+    allSlides[currentSlide].style.display = "none";
+    allSlides[currentSlide-1].style.display = "block";
+    currentSlide -= 1;
+}
+
+function nextSlide() {
+    if (currentSlide == allSlides.length - 1)
+	return
+
+    var allLists = allSlides[currentSlide].querySelectorAll('li');
+    for (var i = 0; i < allLists.length; i++) {
+	if (allLists[i].style.display == "none") {
+	    allLists[i].style.display = "block";
+	    return;
+	}
+    }
+
+    var allP = allSlides[currentSlide].querySelectorAll('p');
+    for (var i = 0; i < allP.length; i++) {
+	if (allP[i].style.display == "none") {
+	    allP[i].style.display = "block";
+	    return;
+	}
+    }
+
+    allSlides[currentSlide].style.display = "none";
+    allSlides[currentSlide+1].style.display = "block";
+    currentSlide += 1;
+}
+
+function goSlide(i) {
+    allSlides[currentSlide].style.display = "none";
+    allSlides[i].style.display = "block";
+    currentSlide = i;
+    hideAll();
 }
     
